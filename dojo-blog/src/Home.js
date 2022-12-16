842111f8c4da5bfd9eadef6179c0b5ef1e873a13
hrudolf <h.rudolf@gmail.com>
2022-12-16 23:36:37 +0100

    Added db api and fetch

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(33);
-    //let name = 'mario';
+    const [blogs, setBlogs] = useState(null);
 
-    const handleClick = (e) => {
-        setName('luigi');
-        setAge('37');
+    const [name, setName] = useState('mario');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log(`Let's fetch!!!`);
+        fetch('http://localhost:8000/blogs')
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data);
+                console.log(data);
+            });
+        console.log('useEffect has just run');
+    }, []);
 
     return (
         <div className="home">
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs && <BlogList blogs={blogs} title={'All blogs!'} handleDelete={handleDelete} />}
+            <button onClick={() => setName('luigi')}>Change name</button>
+            <p>{name}</p>
         </div>
     );
 }
